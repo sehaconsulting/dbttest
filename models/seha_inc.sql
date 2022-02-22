c795d6298521a569fe78ab27bedc57c3ef1df7cd
ali.farid.rizwan@gmail.com <ali.farid.rizwan@gmail.com>
2022-02-22 02:02:15 +0000

    test

--- models/seha_inc.sql
+++ models/seha_inc.sql
@@ -1,5 +1,5 @@
 {{ config(materialized='incremental') }}
-select * from {{ ref('sehanames_perm') }}
+select * from {{ ref('dallasbitest') }}
 {% if is_incremental() %}
 where loaddate > (select max(loaddate) from {{ this }})
 {% endif %}
