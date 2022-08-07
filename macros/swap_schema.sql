7cfc0459203574869c2b66442c8b78509880b93c
ali.farid.rizwan@gmail.com <ali.farid.rizwan@gmail.com>
2022-08-07 05:23:58 +0000

    swap tschema

--- macros/swap_schema.sql
+++ macros/swap_schema.sql
@@ -1,8 +1,8 @@
 # macros/swap_schema.sql
-{% macro swap_schema(tschema) %}
+{% macro swap_schema(sschema, tschema) %}
 
     --{% set sql='alter schema ' ~ tdb ~ '.' ~ tschema ~ ' swap with ' ~ sdb ~ '.' ~ sschema %}
-    {% set sql='alter schema dev.' ~ tschema ~ ' swap with qa.' ~ tschema %}
+    {% set sql='alter schema dev.' ~ sschema ~ ' swap with qa.' ~ tschema %}
     {% do run_query(sql) %}
     {{ log(sql, info=True) }}
 
